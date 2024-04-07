3fc9e5416d37cbf1a3a6d0edcd498b4f4d90bb36
marketplace <sadsain01@gmail.com>
2024-04-07 05:42:25 +0500

    created data input app

---
+++ index.js
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let answer = await inquirer.prompt([
+    {
+        name: "name",
+        type: "input",
+        Message: "What is your name:"
+    },
+    {
+        name: "age",
+        type: "number",
+        Message: "What is your age:"
+    },
+    {
+        name: "gender",
+        type: "list",
+        Message: "What is your gender:",
+        choices: ["male", "female", "prefer not to say"]
+    },
+    {
+        name: "education",
+        type: "input",
+        Message: "What is your education:"
+    }
+]);
+console.log(`Your name is: ${answer.name} \n Your age is: ${answer.age} \n Your gender is: ${answer.gender} \n Your education is: ${answer.education}`);
